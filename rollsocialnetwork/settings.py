5c9eba2fd6f6d3a285fb2ea3b83d2ceaf324c7b0
Douglas Paz <eu@douglaspaz.com>
2023-12-26 23:33:31 -0300

    feat: add phonenumber_field as dep

--- rollsocialnetwork/settings.py
+++ rollsocialnetwork/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
+    "phonenumber_field",
     "rollsocialnetwork.phone_auth",
 ]
 MIDDLEWARE = [
