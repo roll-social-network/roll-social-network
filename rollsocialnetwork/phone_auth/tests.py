0320d3c9d0c00bfc0f457aa660395afd269139b3
Douglas Paz <eu@douglaspaz.com>
2023-12-26 22:15:40 -0300

    feat[phone_auth]: init utils
    - add fill_code
    - add fill_valid_until
    - add normalize_phone_number
    - add get_or_create_user

--- rollsocialnetwork/phone_auth/tests.py
+++ rollsocialnetwork/phone_auth/tests.py
@@ -1,1 +1,25 @@
-# from django.test import TestCase
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .utils import (
+    get_or_create_user,
+    normalize_phone_number,
+)
+
+PHONE_1 = "+55 11 98070-6050"
+PHONE_2 = "+55 11 98070-6051"
+
+class GetOrCreateUserTestCase(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        pn = normalize_phone_number(PHONE_1)
+        self.user_1 = User(**{User.USERNAME_FIELD: pn})
+        self.user_1.save()
+
+    def test_get_ok(self):
+        user = get_or_create_user(PHONE_1)
+        self.assertEqual(user.pk, self.user_1.pk)
+
+    def test_create_ok(self):
+        user = get_or_create_user(PHONE_2)
+        self.assertIsNotNone(user.pk)
+
---
+++ rollsocialnetwork/phone_auth/utils.py
@@ -0,0 +1,45 @@
+"""
+phone auth utils
+"""
+import random
+import string
+from datetime import timedelta
+import phonenumbers
+from django.utils import timezone
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+def fill_code() -> str:
+    """
+    fill code field considering PHONE_AUTH_VALIDATION_CODE_LENGTH setting
+    """
+    return ''.join(random.choice(string.digits) for _ in range(
+        settings.PHONE_AUTH_VALIDATION_CODE_LENGTH))
+
+def fill_valid_until():
+    """
+    fill valid until field considering PHONE_AUTH_VALIDATION_CODE_TTL setting
+    """
+    return timezone.now() + timedelta(minutes=settings.PHONE_AUTH_VALIDATION_CODE_TTL)
+
+def normalize_phone_number(phone_number: str) -> str:
+    """
+    normalize phone number
+    """
+    pn = phonenumbers.parse(phone_number, None)
+    return phonenumbers.format_number(pn, phonenumbers.PhoneNumberFormat.E164)
+
+def get_or_create_user(phone_number: str):
+    """
+    get or create user using phone number
+    """
+    cleaned_phone_number = normalize_phone_number(phone_number)
+    user_model = get_user_model()
+    try:
+        user = user_model.objects.get(**{
+            user_model.USERNAME_FIELD: cleaned_phone_number,
+        })
+    except user_model.DoesNotExist:
+        user = user_model(**{user_model.USERNAME_FIELD: cleaned_phone_number})
+        user.save()
+    return user
