de3b04fbeb0961965f60475e57f6ddadf36e5571
Douglas Paz <eu@douglaspaz.com>
2023-12-26 23:32:09 -0300

    feat[phone_auth]: add PhoneAuthBackend

---
+++ rollsocialnetwork/phone_auth/backends.py
@@ -0,0 +1,30 @@
+"""
+phone auth backends
+"""
+from typing import Any
+from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.http.request import HttpRequest
+from django.contrib.auth import get_user_model
+from .models import VerificationCode
+
+class PhoneAuthBackend(BaseBackend):
+    """
+    phone auth backend class
+    """
+    def authenticate(self,  # pylint: disable=W0221
+                     request: HttpRequest,
+                     phone_number: str | None,
+                     code: str | None,
+                     **kwargs: Any) -> AbstractBaseUser | None:
+        verification_code = VerificationCode.verify(phone_number, code)
+        if verification_code:
+            return verification_code.user
+        return None
+
+    def get_user(self, user_id):
+        user_model = get_user_model()
+        try:
+            return user_model.objects.get(pk=user_id)
+        except user_model.DoesNotExist:
+            return None
