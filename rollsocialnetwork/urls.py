c709735e95c390f8f15820480f4d5c45c58bfaa5
Douglas Paz <eu@douglaspaz.com>
2023-12-30 17:51:53 -0300

    fix: url imports

--- rollsocialnetwork/urls.py
+++ rollsocialnetwork/urls.py
@@ -7,6 +7,8 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from .views import LogoutView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html")),
