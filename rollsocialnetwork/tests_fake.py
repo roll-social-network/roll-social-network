11daf5c248cfab6a8fc7e7b6f83a502b0ccdb2b2
Douglas Paz <eu@douglaspaz.com>
2024-02-12 11:43:24 -0300

    chore[type]: ignore import untyped

--- rollsocialnetwork/tests_fake.py
+++ rollsocialnetwork/tests_fake.py
@@ -2,7 +2,7 @@
 roll tests fake
 """
 from faker import Faker
-from faker_e164.providers import E164Provider
+from faker_e164.providers import E164Provider  # type: ignore[import-untyped]
 
 fake = Faker()
 fake.add_provider(E164Provider)
