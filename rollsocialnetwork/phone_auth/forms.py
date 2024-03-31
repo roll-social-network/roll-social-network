e3cb90313d7e68779a7a4d3319aebbf248b7e77f
Douglas Paz <eu@douglaspaz.com>
2024-03-30 23:23:25 -0300

    feat: create VerifyCodeFormMixin

--- rollsocialnetwork/phone_auth/forms.py
+++ rollsocialnetwork/phone_auth/forms.py
@@ -15,19 +15,10 @@ class RequestVerificationCodeForm(forms.Form):
     phone = PhoneNumberField(label=_('phone number'),
                              help_text=_('+XXXXXXXXXXX, e.g. +1 (212) 5552368'))
 
-class VerifyVerificationCodeForm(forms.Form):
+class VerifyCodeFormMixin:
     """
-    verify verification code form
+    Verify Code Form Mixin
     """
-    phone = PhoneNumberField(label=_('phone number'))
-    code = forms.CharField(max_length=8,
-                           label=_('code'),
-                           help_text=_('sent by sms'))
-
-    error_messages = {
-        "invalid_login": _("Please enter a correct verification code"),
-        "inactive": _("This account is inactive."),
-    }
 
     def __init__(self, *args, request=None, **kwargs):
         self.request = request
@@ -35,13 +26,14 @@ class VerifyVerificationCodeForm(forms.Form):
         super().__init__(*args, **kwargs)
 
     def clean(self):
+        """
+        clean data
+        """
         pn = self.cleaned_data.get("phone")
         phone_number = format_pn(pn)
         code = self.cleaned_data.get("code")
         if phone_number is not None and code:
-            self.user_cache = authenticate(self.request,
-                                           phone_number=phone_number,
-                                           code=code)
+            self.user_cache = self._authenticate(phone_number, code)
             if self.user_cache is None:
                 raise self.get_invalid_login_error()
             self.confirm_login_allowed(self.user_cache)
@@ -71,3 +63,25 @@ class VerifyVerificationCodeForm(forms.Form):
                 self.error_messages["inactive"],
                 code="inactive",
             )
+
+class VerifyVerificationCodeForm(VerifyCodeFormMixin,
+                                 forms.Form):
+    """
+    verify verification code form
+    """
+
+    phone = PhoneNumberField(label=_('phone number'))
+    code = forms.CharField(max_length=8,
+                           label=_('code'),
+                           help_text=_('sent via SMS'))
+
+    error_messages = {
+        "invalid_login": _("please enter a correct verification code"),
+        "inactive": _("this account is inactive."),
+    }
+
+    def _authenticate(self, phone_number, code):
+        return authenticate(self.request,
+                            phone_number=phone_number,
+                            code=code)
+
