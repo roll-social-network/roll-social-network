c6813616bc9f8a800a0eedb88c4c128ce5a78363
Douglas Paz <eu@douglaspaz.com>
2024-05-27 15:04:58 -0300

    feat: add test to SitesViewset and UsersViewset

--- rollsocialnetwork/api/v1/tests.py
+++ rollsocialnetwork/api/v1/tests.py
@@ -3,10 +3,20 @@ api.v1 tests
 """
 from unittest import mock
 import phonenumbers
-from django.test import TestCase
-from django.contrib.auth import get_user_model
+from django.test import (
+    TestCase,
+    RequestFactory,
+)
 from rest_framework.serializers import ValidationError
+from rollsocialnetwork.tests_factory import (
+    UserFactory,
+    SiteFactory,
+)
 from .serializers import VerifyVerificationCodeSerializer
+from .views import (
+    SitesViewset,
+    UsersViewset,
+)
 
 PHONE_NUMBER = "+55 11 98070-6050"
 CODE = "1234"
@@ -15,13 +25,15 @@ class VerifyVerificationCodeSerializerTestCase(TestCase):
     """
     VerifyVerificationCodeSerializer test case
     """
+    def setUp(self):
+        self.user_factory = UserFactory()
+
     @mock.patch("rollsocialnetwork.api.v1.serializers.authenticate")
     def test_validate_ok(self, authenticate_mock):
         """
         .validate ok
         """
-        user_model = get_user_model()
-        user = user_model()
+        user = self.user_factory.create_user()
         authenticate_mock.return_value = user
         serializer = VerifyVerificationCodeSerializer()
         attrs = {
@@ -54,3 +66,39 @@ class VerifyVerificationCodeSerializerTestCase(TestCase):
         attrs = {}
         with self.assertRaises(ValidationError):
             serializer.validate(attrs)
+
+class SitesViewsetTestCase(TestCase):
+    """
+    SitesViewset test case
+    """
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.site_factory = SiteFactory()
+
+    def test_current(self):
+        """
+        current
+        """
+        request = self.factory.get('/api/v1/sites/current/')
+        request.site = self.site_factory.create_site()
+        response = SitesViewset(request=request,
+                                format_kwarg={}).current(request)
+        self.assertEqual(response.status_code, 200)
+
+class UsersViewsetTestCase(TestCase):
+    """
+    UsersViewset test case
+    """
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user_factory = UserFactory()
+
+    def test_current(self):
+        """
+        current
+        """
+        request = self.factory.get('/api/v1/users/current/')
+        request.user = self.user_factory.create_user()
+        response = UsersViewset(request=request,
+                                format_kwarg={}).current(request)
+        self.assertEqual(response.status_code, 200)
